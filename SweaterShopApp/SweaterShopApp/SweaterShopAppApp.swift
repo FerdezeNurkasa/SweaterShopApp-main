862cfc7e4ba6ac155963c0158de5f1d55a684d37
Fortesa Cena <fortesa.cena@student.uni-pr.edu>
2024-02-23 22:38:10 -0800

    Create-an-initial-Xcode-project

---
+++ SweaterShopApp/SweaterShopApp/SweaterShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweaterShopAppApp.swift
+//  SweaterShopApp
+//
+//  Created by Fortesa Cena on 23.2.24.
+//
+
+import SwiftUI
+
+@main
+struct SweaterShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
