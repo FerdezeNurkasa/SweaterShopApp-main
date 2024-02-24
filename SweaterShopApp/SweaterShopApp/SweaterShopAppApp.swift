fcade4e7fd31aa95ae6d2b0745eb12dce677675b
Fortesa Cena <fortesa.cena@student.uni-pr.edu>
2024-02-23 23:03:55 -0800

    managing-shopping-cart-and-processing-payments

--- SweaterShopApp/SweaterShopApp/SweaterShopAppApp.swift
+++ SweaterShopApp/SweaterShopApp/SweaterShopAppApp.swift
@@ -2,16 +2,19 @@
 //  SweaterShopAppApp.swift
 //  SweaterShopApp
 //
-//  Created by Fortesa Cena on 23.2.24.
-//
+//  Created by Ferdeze Nurkasa on 24.2.24.
 
 import SwiftUI
 
-@main
-struct SweaterShopAppApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
-}
+//@main
+//struct SweaterShopAppApp: App {
+    //let persistenceController = PersistenceController.shared
+
+
+   // var body: some Scene {
+       // WindowGroup {
+            //ContentView()
+              //  //.environment(\//.managedObjectContext, //persistenceController//.container.viewContext)
+       // }
+  //  }
+//}
