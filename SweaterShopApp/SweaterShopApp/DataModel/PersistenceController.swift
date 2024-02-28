a4e44a92d8406538c6946316322710803010c0a5
Fortesa Cena <121317246+Fortesacena@users.noreply.github.com>
2024-02-28 14:26:06 -0800

    Model and data layer (#3)
    * Add-Data-Files-And-Models
    
    * Add-Core-Data-Files
    
    * Add-Cart-Management-And-Payment-Handling

---
+++ SweaterShopApp/SweaterShopApp/DataModel/PersistenceController.swift
@@ -0,0 +1,40 @@
+//
+//  PersistenceController.swift
+//  SweaterShopApp
+//
+//  Created by Fortesa Cena on 27.2.24.
+//
+
+import Foundation
+import CoreData
+
+struct PersistenceController {
+    static let shared = PersistenceController()
+
+    let container: NSPersistentContainer
+
+    init() {
+        container = NSPersistentContainer(name: "SweaterShopModel") // Ensure this name matches your Data Model file's name
+        container.loadPersistentStores { (storeDescription, error) in
+            if let error = error as NSError? {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+    }
+    
+    func saveContext () {
+        let context = container.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
